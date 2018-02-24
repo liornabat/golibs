8f8ba2ae4b1004bcbe22c031fa21ecbfe9529f9a
lior.nabat <lior.nabat@tradency.com>
2018-02-24 16:03:39 +0200

    add helpers to metrics

--- metrics/factory.go
+++ metrics/factory.go
@@ -14,6 +14,10 @@
 
 package metrics
 
+import (
+	"github.com/liornabat/golibs/cache"
+)
+
 // Factory creates new metrics
 type Factory interface {
 	Counter(name string, tags map[string]string) Counter
@@ -33,3 +37,62 @@ func (nullFactory) Counter(name string, tags map[string]string) Counter   { retu
 func (nullFactory) Timer(name string, tags map[string]string) Timer       { return NullTimer }
 func (nullFactory) Gauge(name string, tags map[string]string) Gauge       { return NullGauge }
 func (nullFactory) Namespace(name string, tags map[string]string) Factory { return NullFactory }
+
+type MetricsFactory struct {
+	metricsCache *cache.LRU
+	promFactory  *PrometheusFactory
+}
+
+func NewMetricsFactory() *MetricsFactory {
+	return &MetricsFactory{
+		metricsCache: cache.NewLRU(1000),
+		promFactory:  New(),
+	}
+}
+
+func (mf *MetricsFactory) SetNamespace(name string, tags map[string]string) *MetricsFactory {
+	mf.promFactory.Namespace(name, tags)
+	return mf
+}
+
+func (mf *MetricsFactory) AddCounter(key, name string, tags map[string]string) *Counter {
+	c := mf.promFactory.Counter(name, tags)
+	mf.metricsCache.Put(key, &c)
+	return &c
+}
+
+func (mf *MetricsFactory) AddGauge(key, name string, tags map[string]string) *Gauge {
+	g := mf.promFactory.Gauge(name, tags)
+	mf.metricsCache.Put(key, &g)
+	return &g
+}
+
+func (mf *MetricsFactory) AddTimer(key, name string, tags map[string]string) *Timer {
+	t := mf.promFactory.Timer(name, tags)
+	mf.metricsCache.Put(key, &t)
+	return &t
+}
+
+func (mf *MetricsFactory) GetCounter(key string) *Counter {
+	v, ok := mf.metricsCache.Get(key).(*Counter)
+	if ok {
+		return v
+	}
+	return nil
+}
+
+func (mf *MetricsFactory) GetGauge(key string) *Gauge {
+	v, ok := mf.metricsCache.Get(key).(*Gauge)
+	if ok {
+		return v
+	}
+	return nil
+
+}
+func (mf *MetricsFactory) GetTimer(key string) *Timer {
+	v, ok := mf.metricsCache.Get(key).(*Timer)
+	if ok {
+		return v
+	}
+	return nil
+}
